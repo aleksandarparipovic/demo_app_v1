b9a1efad991e758bd3f523005bfd5968f3a0f3c6
Aleksandar Paripovic <aleksandarparipovic@192.168.1.10>
2024-08-26 02:23:31 +0200

    Changed HomeView page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import APIRequestView from './APIRequestView.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <APIRequestView/>
   </main>
 </template>
